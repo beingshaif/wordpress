39995830af0501a06b153a51b96d179c7543763a
Saif <saif@forwardcode.in>
2022-03-23 19:08:30 +0530

    teammembers

--- wp-content/themes/basic-theme/single-teammembers.php
+++ wp-content/themes/basic-theme/single-teammembers.php
@@ -2,6 +2,8 @@
 
 $locations = get_field('locations');
 
+$related_posts = get_field('related_posts');
+
 ?>
 
 
@@ -11,9 +13,13 @@ $locations = get_field('locations');
         <h1><?php the_title(); ?></h1>
         <hr>
 
-        <?php foreach ($locations as $location) : ?>
+        <?php foreach ($locations as $post) : ?>
 
-            <h3>
+            <?php setup_postdata($post); ?>
+            <h3><?php echo the_title(); ?></h3>
+            <p><?php the_field('address'); ?></p>
+
+            <!-- <h3>
                 <a href="<?php echo get_page_link($location->ID)  ?>">
                     <?php echo $location->post_title ?>
                 </a>
@@ -22,8 +28,37 @@ $locations = get_field('locations');
             <img src="<?php echo get_the_post_thumbnail_url($location->ID, 'my_custom_size') ?>" class="img-fluid m-3 img-thumbnail" alt="<?php the_title(); ?>">
             <hr>
 
-            <p><?php echo $location->post_content ?></p>
-        <?php endforeach; ?>
+            <p><?php echo $location->post_content ?></p> -->
+
+        <?php wp_reset_postdata();
+        endforeach; ?>
+
+        <!-- <?php echo the_title(); ?> -->
+
+        <hr>
+        
+        <?php
+        //  echo '<pre>';
+        // print_r($related_posts);
+        // echo '</pre>'; 
+        ?>
+        <?php if ($related_posts) : ?>
+
+
+
+            <?php foreach ($related_posts as $related_post) : ?>
+
+                <a href="<?php echo get_the_permalink($related_post->ID) ?>">
+                    <h3> <?php echo $related_post->post_title; ?></h3>
+                </a>
+                <p>
+                    <?php echo $related_post->post_content ?>
+                </p>
+                <hr>
+            <?php endforeach; ?>
+
+        <?php endif; ?>
+
 
     </div>
 </section>
