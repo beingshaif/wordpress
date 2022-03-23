27e4df5b37ca291938c2e5d4252645c5a6e81ece
Saif <saif@forwardcode.in>
2022-03-23 19:08:37 +0530

    single

--- wp-content/themes/basic-theme/single.php
+++ wp-content/themes/basic-theme/single.php
@@ -9,7 +9,6 @@
         <h1><?php the_title(); ?></h1>
 
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <img src="<?php the_post_thumbnail_url('my_custom_size') ?>" class="img-fluid m-3 img-thumbnail" alt="<?php the_title(); ?>">
         <hr>
 
         <?php the_content(); ?>
